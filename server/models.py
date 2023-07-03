2d8679cde4cda74562f3844da181b823d6afc5ae
BETHWEL <bethwelk85@gmail.com>
2023-07-03 14:57:26 +0300

    done

--- server/models.py
+++ server/models.py
@@ -6,3 +6,24 @@ metadata = MetaData(naming_convention={
 })
 
 db = SQLAlchemy(metadata=metadata)
+
+class Owner(db.Model):
+    __tablename__ ='owners'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.column(db.String)
+    
+    pets = db.relationship('Pet', backref='owner')
+
+    def __repr__(self):
+        return f'<Pet Owner {self.name}>'
+
+class Pets(db.Model):
+    __tablename__ ='pets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    species = db.column(db.String)
+    owner = db.column(db.String, db.ForeignKey('owners.id'))
+
+    def __repr__(self):
+        return f'<Pet {self.name}, {self.species}>'    
